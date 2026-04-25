6755d5d6299e38d148c9dc67e5504712119fe6c0
Yevhen Khudoliiv <eugenkhudoliiv@gmail.com>
2026-04-25 19:21:02 +0200

    lesson-8: root main.tf with helm/k8s providers + module wiring

---
+++ lesson-8/outputs.tf
@@ -0,0 +1,59 @@
+output "state_bucket_name" {
+  description = "S3 bucket holding Terraform state"
+  value       = module.s3_backend.bucket_name
+}
+
+output "lock_table_name" {
+  description = "DynamoDB table for state locking"
+  value       = module.s3_backend.dynamodb_table_name
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = module.vpc.private_subnet_ids
+}
+
+output "ecr_repository_url" {
+  description = "ECR repository URL (docker push target)"
+  value       = module.ecr.repository_url
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS API server endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "kubeconfig_command" {
+  description = "Command to configure kubectl"
+  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.aws_region}"
+}
+
+output "jenkins_url_command" {
+  description = "Command to get Jenkins LoadBalancer hostname"
+  value       = module.jenkins.jenkins_url_command
+}
+
+output "argocd_server_command" {
+  description = "Command to get Argo CD server hostname"
+  value       = module.argo_cd.argocd_server_command
+}
+
+output "argocd_initial_password_command" {
+  description = "Command to get initial Argo CD admin password"
+  value       = module.argo_cd.argocd_initial_password_command
+}
