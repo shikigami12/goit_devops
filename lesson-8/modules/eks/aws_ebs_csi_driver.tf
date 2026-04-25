b8a6f81315bfc95aeacb114cd4b2db6762c0e336
Yevhen Khudoliiv <eugenkhudoliiv@gmail.com>
2026-04-25 19:17:18 +0200

    lesson-8: copy eks module + add ebs-csi-driver addon

---
+++ lesson-8/modules/eks/aws_ebs_csi_driver.tf
@@ -0,0 +1,43 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  oidc_provider = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
+}
+
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "${var.cluster_name}-ebs-csi-driver"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_provider}"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${local.oidc_provider}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+            "${local.oidc_provider}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.cluster_name}-ebs-csi-driver"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  role       = aws_iam_role.ebs_csi_driver.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
+}
---
+++ lesson-8/modules/eks/variables.tf
@@ -0,0 +1,44 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for the EKS control plane"
+  type        = string
+  default     = "1.30"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC the cluster lives in"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnet IDs for cluster ENIs and managed node group instances (typically private subnets)"
+  type        = list(string)
+}
+
+variable "node_instance_types" {
+  description = "EC2 instance types for the managed node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "node_desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "node_max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
